bdddfdcaaa4c0811295dc4defdf4158ec8492f5c
eandcastro <eanj.dcastro@gmail.com>
2023-05-25 23:08:26 +0800

    fix: remove unused import

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,7 +6,6 @@ import {
   Patch,
   Param,
   Delete,
-  ValidationPipe,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
